b031baba13def571ad1809adaf7782e3075f8f46
martistuwe <martistuwe@yahoo.de>
2020-08-09 08:37:01 +0200

    updated dependency to lwjgl3

--- desktop/src/de/fhbielefeld/pmdungeon/desktop/DesktopLauncher.java
+++ desktop/src/de/fhbielefeld/pmdungeon/desktop/DesktopLauncher.java
@@ -1,12 +1,13 @@
 package de.fhbielefeld.pmdungeon.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import de.fhbielefeld.pmdungeon.PMDungeon;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new PMDungeon(), config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setTitle("PM-Dungeon");
+		new Lwjgl3Application(new PMDungeon(), config);
 	}
 }
