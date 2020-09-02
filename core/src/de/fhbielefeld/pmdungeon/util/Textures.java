6fa07fcd43ac4ece8d86a896ea482bf001e614cf
martistuwe <martistuwe@yahoo.de>
2020-09-02 11:17:58 +0200

    added textures enum for central texture handling

---
+++ core/src/de/fhbielefeld/pmdungeon/util/Textures.java
@@ -0,0 +1,31 @@
+package de.fhbielefeld.pmdungeon.util;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.ObjectMap;
+
+public enum Textures {
+    FLOOR("textures/dungeon/floor/floor_1.png"),
+    WALL_MID("textures/dungeon/wall/wall_mid.png");
+
+    private final String filename;
+
+    Textures(String filename) {
+        this.filename = filename;
+    }
+
+    protected Texture get() {
+        if (filename != null) {
+            return new Texture(filename);
+        } else {
+            throw new IllegalArgumentException("No texture found for filename.");
+        }
+    }
+
+    public static ObjectMap<Textures, Texture> loadAllTextures() {
+        ObjectMap<Textures, Texture> textureMap = new ObjectMap<>();
+        for (Textures t : Textures.values()) {
+            textureMap.put(t, t.get());
+        }
+        return textureMap;
+    }
+}
