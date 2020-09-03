57d5b53b9590867f3087dca8e865830f39bd560b
martistuwe <martistuwe@yahoo.de>
2020-09-03 20:04:51 +0200

    added black texture to textures enum

--- core/src/de/fhbielefeld/pmdungeon/util/Textures.java
+++ core/src/de/fhbielefeld/pmdungeon/util/Textures.java
@@ -1,5 +1,6 @@
 package de.fhbielefeld.pmdungeon.util;
 
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.utils.ObjectMap;
 
@@ -18,10 +19,23 @@ public enum Textures {
     WALL_SIDE_MID_RIGHT("wall/wall_side_mid_right.png"),
     WALL_SIDE_TOP_RIGHT("wall/wall_side_top_right.png"),
     WALL_TOP_MID("wall/wall_top_mid.png"),
-    ;
+    BLACK_PIXEL() {
+        @Override
+        public Texture get() {
+            Pixmap blackPixelPixmap = new Pixmap(1, 1, Pixmap.Format.RGB888);
+            blackPixelPixmap.setColor(0f, 0f, 0f, 1f);
+            blackPixelPixmap.fill();
+            Texture blackPixel = new Texture(blackPixelPixmap);
+            blackPixelPixmap.dispose();
+            return blackPixel;
+        }
+    };
 
     private static final String PATH = "textures/dungeon/";
-    private final String filename;
+    private String filename = null;
+
+    Textures() {
+    }
 
     Textures(String filename) {
         this.filename = filename;
