a4559a5cf091b30ae73dad72b768034bc6857062
martistuwe <martistuwe@yahoo.de>
2020-09-18 15:59:58 +0200

    added chest to gameworld

--- core/src/de/fhbielefeld/pmdungeon/game/GameWorld.java
+++ core/src/de/fhbielefeld/pmdungeon/game/GameWorld.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.utils.Disposable;
 import de.fhbielefeld.pmdungeon.game.characters.Character;
 import de.fhbielefeld.pmdungeon.game.characters.Imp;
 import de.fhbielefeld.pmdungeon.game.characters.MaleKnight;
+import de.fhbielefeld.pmdungeon.game.chest.Chest;
 import de.fhbielefeld.pmdungeon.game.dungeon.Dungeon;
 import de.fhbielefeld.pmdungeon.game.dungeon.dungeonconverter.DungeonConverter;
 import de.fhbielefeld.pmdungeon.game.inputhandling.Command;
@@ -17,6 +18,7 @@ public class GameWorld implements Disposable {
 
     private final SpriteBatch batch;
     private final List<Character> characterList = new ArrayList<>();
+    private final List<Chest> chestList = new ArrayList<>();
     private final InputHandler inputHandler = new InputHandler();
     private Dungeon dungeon;
     private Character hero;
@@ -26,6 +28,7 @@ public class GameWorld implements Disposable {
 
         setupDungeon();
         setupCharacters();
+        setupLoot();
     }
 
     private void setupDungeon() {
@@ -43,6 +46,10 @@ public class GameWorld implements Disposable {
         characterList.add(imp);
     }
 
+    private void setupLoot() {
+        chestList.add(new Chest(dungeon.getRandomPointInDungeon()));
+    }
+
     public void update() {
         Command[] commands = inputHandler.handleInput();
         if (commands.length == 0) hero.setIdle(true);
@@ -52,6 +59,9 @@ public class GameWorld implements Disposable {
                 command.execute(hero);
             }
         }
+        for (Chest chest : chestList) {
+            chest.update();
+        }
         for (Character character : characterList) {
             character.update();
         }
@@ -59,6 +69,9 @@ public class GameWorld implements Disposable {
 
     public void render() {
         dungeon.renderFloor(batch);
+        for (Chest chest : chestList) {
+            chest.render(batch);
+        }
         for (Character character : characterList) {
             character.render();
         }
