74a4e495697319901dc7e2b1f69f6e3157b8df40
martistuwe <martistuwe@yahoo.de>
2020-09-19 09:30:32 +0200

    moved demon textures

--- core/src/de/fhbielefeld/pmdungeon/game/characters/Imp.java
+++ core/src/de/fhbielefeld/pmdungeon/game/characters/Imp.java
@@ -15,8 +15,8 @@ public class Imp extends Character {
         this.idleAnimation = new Animation(0.2f);
         this.runAnimation = new Animation(0.2f);
         for (int i = 0; i < 4; i++) {
-            this.idleAnimation.addTexture(new Texture("textures/characters/nonplayercharacters/demons/imp/imp_idle_anim_f" + i + ".png"));
-            this.runAnimation.addTexture(new Texture("textures/characters/nonplayercharacters/demons/imp/imp_run_anim_f" + i + ".png"));
+            this.idleAnimation.addTexture(new Texture("textures/characters/demons/imp/imp_idle_anim_f" + i + ".png"));
+            this.runAnimation.addTexture(new Texture("textures/characters/demons/imp/imp_run_anim_f" + i + ".png"));
         }
     }
 
